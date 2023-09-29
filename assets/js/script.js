897c1f67aeb20d35590aa76bbbe292db87662c45
Alex M <alexmajors0613@gmail.com>
2023-09-29 10:40:09 -0500

    add submitScore fucntion and scores page

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,8 @@ var answer2 = answers.children[1];
 var answer3 = answers.children[2];
 var answer4 = answers.children[3];
 var endCard = document.querySelector("#endCard");
-var finalScore = document.querySelector("#endCard").children[1];
 var correctDisplay = document.querySelector("#correct");
+var submit = document.querySelector("#submit")
 var unusedQuestions = [1,2,3,4,5];
 var score = 75;
 var scoreTimer = null;
@@ -61,29 +61,134 @@ function endQuiz() {
     questionCard.setAttribute("style", "display:none");
     timerEl.textContent = "Time: " + score;
     clearInterval(scoreTimer);
-    finalScore.textContent= "Your final score is: " + score;
+    endCard.children[1].textContent= "Your final score is: " + score;
 }
 
-//TODO: add submit score functionality
+function submitScore(event) {
+    event.preventDefault();
+    var input = document.querySelector("#initials");
+    var newScore = input.value + " - " + score;
+    var oldScores = (JSON.parse(localStorage.getItem("scores")));
+    if (oldScores == null) {
+        oldScores = [newScore];
+        console.log("oldScores new: " + oldScores);
+        localStorage.setItem("scores",JSON.stringify(oldScores));
+        window.location.href = "scores.html";
+        return
+    } else if (oldScores.length == 1) {
+        oldScores = Array.from(JSON.parse(localStorage.getItem("scores")))
+        var thisScore = JSON.stringify(oldScores[0]);
+        console.log("thisScore len 1: " + thisScore);
+        var digit1Place = thisScore.length - 3;
+        var digit1 = thisScore.charAt(digit1Place);
+        console.log(digit1);
+        var digit2Place = thisScore.length - 2;
+        var digit2 = thisScore.charAt(digit2Place);
+        console.log(digit2);
+        var prevScore = parseInt(digit1 + digit2);
+        console.log(prevScore + " " +prevScore.type);
+        if (score >= prevScore){
+            oldScores.unshift(newScore);
+            localStorage.setItem("scores",JSON.stringify(oldScores));
+            window.location.href = "scores.html";
+            return
+        }
+        else {
+            oldScores.push(newScore);
+            localStorage.setItem("scores",JSON.stringify(oldScores));
+            window.location.href = "scores.html";
+            return
+        }
+    } else {
+        oldScores = Array.from(JSON.parse(localStorage.getItem("scores")))
+        var len = oldScores.length
+        for (i = 0; i < len; i ++) {
+            var thisScore = JSON.stringify(oldScores[i]);
+            console.log("thisScore len i: " + thisScore + " i = " + i);
+            var digit1Place = thisScore.length - 3;
+            var digit1 = thisScore.charAt(digit1Place);
+            console.log(digit1);
+            var digit2Place = thisScore.length - 2;
+            var digit2 = thisScore.charAt(digit2Place);
+            console.log(digit2);
+            var prevScore = parseInt(digit1 + digit2);
+            console.log(prevScore + " " +prevScore.type);
+            if (score >= prevScore) {
+                if (i == 0) {
+                    var allScores = [newScore, ...oldScores.slice(0)];
+                    oldScores = allScores;
+                    console.log("oldScores len i: " + oldScores + " " + " i = " + i);
+                    while (oldScores.length > 10) {
+                        oldScores.pop();
+                        console.log("oldScores too long: " + oldScores);
+                    }
+                    localStorage.setItem("scores",JSON.stringify(oldScores));
+                    window.location.href = "scores.html";
+                    return
+                } else {
+                    var allScores = [...oldScores.slice(0,i), newScore, ...oldScores.slice(i)];
+                    oldScores = allScores;
+                    console.log("oldScores len i: " + oldScores + " " + " i = " + i);
+                    while (oldScores.length > 10) {
+                        oldScores.pop();
+                        console.log("oldScores too long: " + oldScores);
+                    }
+                    localStorage.setItem("scores",JSON.stringify(oldScores));
+                    window.location.href = "scores.html";
+                    return
+                }
+            }
+        }
+        oldScores.push(newScore);
+        console.log("oldScores lowest: " + oldScores);
+        while (oldScores.length > 10) {
+            oldScores.pop();
+            console.log("oldScores too long: " + oldScores);
+        }
+        localStorage.setItem("scores",JSON.stringify(oldScores));
+        window.location.href = "scores.html";
+        return
+    }
+}
+
+/* function submitScore(event) {
+    event.preventDefault();
+    var input = document.querySelector("#initials");
+    var newScore = input.value + " - " + score;
+    var oldScores = JSON.parse(localStorage.getItem("scores"));
+    console.log("oldScores1: " + oldScores + localStorage.getItem("scores"));
+    if (oldScores == null) {
+        oldScores = [newScore];
+        console.log("oldScores2: " + oldScores);
+        localStorage.setItem("scores",JSON.stringify(oldScores));
+    } else {
+        console.log("oldScores3: " + oldScores);
+        oldScores.push(newScore);
+        console.log("oldScores4: " + oldScores);
+        localStorage.setItem("scores",JSON.stringify(oldScores));
+    }
+} */
 
 function checkAnswer(event) {
     var answer = event.target;
-    console.log(answer)
+    // console.log("Selected answer: " + answer.textContent)
     if (answer.dataset.correct === "true") {
         correctDisplay.setAttribute("style", "display:block");
         correctDisplay.textContent = "Correct";
+        // console.log("Correct")
     } else {
         correctDisplay.setAttribute("style", "display:block");
         correctDisplay.textContent = "Incorrect";
         score -= 10
         timerEl.textContent = "Time: " + score;
+        // console.log("Incorrect")
     }
     randomQuestion();
 }
 
 function question1() {
     question.dataset.number = 1;
-    console.log("Question number: " + question.dataset.number)
+    // console.log("Question number: " + question.dataset.number)
     question.textContent = "Commonly used data types do NOT include: ";
     answer1.textContent = "strings";
     answer2.textContent = "booleans";
@@ -97,7 +202,7 @@ function question1() {
 
 function question2() {
     question.dataset.number = 2;
-    console.log("Question number: " + question.dataset.number)
+    // console.log("Question number: " + question.dataset.number)
     question.textContent = "The condition in an if / else statement is enclosed with _______.";
     answer1.textContent = "quotes";
     answer2.textContent = "curly brackets";
@@ -111,7 +216,7 @@ function question2() {
 
 function question3() {
     question.dataset.number = 3;
-    console.log("Question number: " + question.dataset.number)
+    // console.log("Question number: " + question.dataset.number)
     question.textContent = "Arrays in JavaScript can be used to store _______.";
     answer1.textContent = "numbers and strings";
     answer2.textContent = "other arrays";
@@ -125,7 +230,7 @@ function question3() {
 
 function question4() {
     question.dataset.number = 4;
-    console.log("Question number: " + question.dataset.number)
+    // console.log("Question number: " + question.dataset.number)
     question.textContent = "String values must be enclosed within ______ when being assigned to variables.";
     answer1.textContent = "commas";
     answer2.textContent = "curly brackets";
@@ -139,8 +244,8 @@ function question4() {
 
 function question5() {
     question.dataset.number = 5;
-    console.log("Question number: " + question.dataset.number)
-    question.textContent = "A verry useful tool used during development and debugging for printing content to the debugger is: ";
+    // console.log("Question number: " + question.dataset.number)
+    question.textContent = "A verry useful tool used during development and debugging for printing content to the debugger is:";
     answer1.textContent = "JavaScript";
     answer2.textContent = "terminal/bash";
     answer3.textContent = "for loops";
@@ -154,3 +259,4 @@ function question5() {
 init();
 startButton.addEventListener("click",startQuiz);
 answers.addEventListener("click",checkAnswer);
+submit.addEventListener("click",submitScore);
